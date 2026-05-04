940885d454e3df253707b724a3addcf9c9cb162a
akanala <ashrith-kanala@uiowa.edu>
2026-05-03 19:39:27 -0500

    Organized automation tests into automation folder

---
+++ automation/CourseTracker.Tests/Test1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CourseTracker.Tests;
+
+[TestClass]
+public class GpaTests
+{
+    [TestMethod]
+    public void CalculateGpa_ValidCourses_ReturnsCorrectValue()
+    {
+        // Arrange
+        double totalPoints = (3 * 4.0) + (3 * 3.0);
+        int totalCredits = 6;
+
+        // Act
+        double gpa = totalPoints / totalCredits;
+
+        // Assert
+        Assert.AreEqual(3.5, gpa);
+    }
+}
